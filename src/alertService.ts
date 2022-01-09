34353a39f2b7194df5791fc64a2fc71130fd1554
Johnny Almonte <johnny243@users.noreply.github.com>
2022-01-08 22:03:26 -0400

    fix: output confirm and alert to console

--- src/alertService.ts
+++ src/alertService.ts
@@ -4,13 +4,11 @@ export default class WebAlertService implements SNAlertService {
   confirm(text: string, title?: string, confirmButtonText?: string, confirmButtonType?: ButtonType, cancelButtonText?: string): Promise<boolean> {
     console.log(text);
     return
-    //throw new Error('Method not implemented.');
   }
 
   alert(text: string, title?: string, closeButtonText?: string): Promise<void> {
     console.log(text);
-    return
-    //throw new Error('Method not implemented.');
+    return;
   }
 
   blockingDialog(): DismissBlockingDialog | Promise<DismissBlockingDialog> {
